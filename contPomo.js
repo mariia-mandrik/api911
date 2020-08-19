8175c7261a3991fceb5aeb25bbe08569f1184aa4
Mariia Mandrik <manyanyka462@gmail.com>
2020-08-19 15:34:12 +0300

    add controler

---
+++ contPomo.js
@@ -0,0 +1,17 @@
+// /////////////////////////////////////////////////////////////////////////////
+// getPharmaciesIdBalance
+// /////////////////////////////////////////////////////////////////////////////
+controllers.getPromosFromSite = async (req, h) => {
+   // const { id: pharmacyId } = req.params;
+    //const { limit, offset } = req.query;
+    try {
+        const res = await repository.getPromosFromSite();
+        if (res) {
+            return h.response(res);
+        }
+        throw Boom.badRequest('No result');
+    } catch (err) {
+        console.error(`:ERROR get promos  failed! ${moment().format()} ${err}`);
+        return h.response({ ...err, message: `:ERROR get promosgi failed!` }).code((err.output && err.output.statusCode) || 499);
+    }
+};
