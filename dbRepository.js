74ea4639b656eb53af417924d2f1c328a02e150f
Mariia Mandrik <manyanyka462@gmail.com>
2020-08-19 16:05:46 +0300

    add db

---
+++ dbRepository.js
@@ -0,0 +1,23 @@
+// /////////////////////////////////////////////////////////////////////////////
+// ТЕКУЩИЕ ОСТАТКИ В АПТЕКЕ getPharmaciesIdBalance
+// /////////////////////////////////////////////////////////////////////////////
+async function getPromosFromSite(objArgs) {
+    //const { pharmacyId, offset = 0, limit = DEFAULT_PHARMACY_BALANCE } = objArgs;
+
+    try {
+        const pool = await poolPromise;
+        const res = await pool.request()
+            //.input('pharmacyId', sql.Int, pharmacyId)
+            //.input('offset', sql.Int, offset)
+            //.input('limit', sql.Int, limit)
+            .execute('dbo.p_api_GetPharmaciesIdBalance');//change function
+
+        if (res && res.recordset && res.returnValue === 0) {
+            return res.recordset;
+        }
+        throw Boom.badRequest(`No result`);
+    } catch (err) {
+        throw Boom.boomify(err, `error`);
+    }
+}
+
